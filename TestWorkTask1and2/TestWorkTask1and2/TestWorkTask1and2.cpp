fafefe55bc09e020e01e4a4072ddf1f16995db93
akchurinabarbara <48187669+akchurinabarbara@users.noreply.github.com>
2023-05-17 20:38:50 +0300

    Output fix

--- TestWorkTask1and2/TestWorkTask1and2/TestWorkTask1and2.cpp
+++ TestWorkTask1and2/TestWorkTask1and2/TestWorkTask1and2.cpp
@@ -11,17 +11,17 @@ int main()
     unsigned x = 98;
 
     std::cout << "Task 1\n";
-    std::cout << weight1(x)<<"\n";
-    std::cout << weight2(x) << "\n";
-    std::cout << weight3(x) << "\n";
-    std::cout << weight4(x) << "\n";
-    std::cout << weight5(x) << "\n";
+    std::cout << "method 1: " << weight1(x) << "\n";
+    std::cout << "method 2: " << weight2(x) << "\n";
+    std::cout << "method 3: " << weight3(x) << "\n";
+    std::cout << "method 4: " << weight4(x) << "\n";
+    std::cout << "method 5: " << weight5(x) << "\n";
 
     std::cout << "\nTask 2\n";
-    std::cout << parity1(x) << "\n";
-    std::cout << parity2(x) << "\n";
-    std::cout << parity3(x) << "\n";
-    std::cout << parity4(x) << "\n";
-    std::cout << parity5(x) << "\n";
+    std::cout << "method 1: " << parity1(x) << "\n";
+    std::cout << "method 2: " << parity2(x) << "\n";
+    std::cout << "method 3: " << parity3(x) << "\n";
+    std::cout << "method 4: " << parity4(x) << "\n";
+    std::cout << "method 5: " << parity5(x) << "\n";
 }
 
